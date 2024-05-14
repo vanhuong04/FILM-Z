134fd5e02cc0164fd78c1e9b0f216227cfb6fd07
vanhuong04 <vanhuong100704@gmail.com>
2024-05-14 20:48:44 +0700

    test2

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,10 @@
-import { header2 } from "./page/category/category.js"
-import { action } from "./page/home/action.js"
-import { anime } from "./page/home/anime.js"
-import { category } from "./page/home/category.js"
-import { slider } from "./page/home/slider.js"
-import { TV } from "./page/home/tvShows.js"
-import { vietnam } from "./page/home/vietnam.js"
+import { header2 } from "../javascript/page/category/category.js"
+import { action } from "../javascript/page/home/action.js"
+import { anime } from "../javascript/page/home/anime.js"
+import { category } from "../javascript/page/home/category.js"
+import { slider } from "../javascript/page/home/slider.js"
+import { TV } from "../javascript/page/home/tvShows.js"
+import { vietnam } from "../javascript/page/home/vietnam.js"
 import { search } from "./search"
 
 
